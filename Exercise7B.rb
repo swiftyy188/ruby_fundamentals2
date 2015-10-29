3dd1285233e0829a61e6c060c8b5edf7826f8f00
swiftyy188 <jeffho.ml@gmail.com>
2015-10-29 09:26:54 -0400

    Exercise7B

---
+++ Exercise7B.rb
@@ -0,0 +1,28 @@
+#Q1 good
+students = { 
+	:cohort1 => 34,
+	:cohort2 => 42,
+	:cohort3 => 22
+}
+#def print_students(students)
+#students.each do |key, value|
+#	puts "#{key}: #{value} students"
+#	end
+
+#end
+#print_students(students)
+
+#Q2 good
+
+#students[:cohort4] = 43
+#print_students(students)
+
+#Q3
+puts students.keys
+#Q4
+#students[:cohort5] = ""
+
+#Q5
+#students.remove(:cohort2)
+
+#puts students
